5bba5e2890d006d8d104c3b041edb679214e48e9
yan <yan@gmail.com>
2017-03-09 23:53:43 +0800

    join create database and table's each configuration

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,43 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int proviceId;
+    private int cityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getProviceId() {
+        return proviceId;
+    }
+
+    public void setProviceId(int proviceId) {
+        this.proviceId = proviceId;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+}
